0b0e90fd0cbffd39348786659fbff8e471a8f97c
Komi <komasadi@gmail.com>
2021-05-19 01:54:16 +0430

    bakhshe projects dar ghesmate index be sorate partial anjam shod

---
+++ DevCard_MVC/Models/Project.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+    }
+
+    public class Article
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class IndexModel
+    {
+       public List<Project> Projects { get; set; }
+
+        public List<Article> Articles { get; set; }
+    }
+        
+        
+}
