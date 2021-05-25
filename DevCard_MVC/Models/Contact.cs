54aabb079e967a93315ed1dd829fd85244abb9ba
Komi <komasadi@gmail.com>
2021-05-26 00:50:36 +0430

    Contact form client side validation impelement

---
+++ DevCard_MVC/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_MVC.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage ="این فبلد اجباری است")]
+        [MinLength(3,ErrorMessage ="حداقل طول نام 3 کاراکتر است")]
+        [MaxLength(100 ,ErrorMessage ="حداکثر طول نام 100 کاراکتر است")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "این فبلد اجباری است")]
+        [EmailAddress(ErrorMessage ="مقدار وارد شده ایمیل صحیح نیست")]
+        public string Email { get; set; }
+
+        
+        public string Message { get; set; }
+
+        [Required(ErrorMessage = "این فبلد اجباری است")]
+        public string Service { get; set; }
+
+       
+    }
+}
